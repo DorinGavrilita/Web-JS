b4de88b949b1b8ebceb39f2cd7568d93794c6ac9
DorinGavrilita <gavrilitadorin5@gmail.com>
2023-02-28 00:00:22 +0200

    Variables

---
+++ JSBasics/2 - Variables/app.js
@@ -0,0 +1,10 @@
+let admin, name;
+name = "Джон";
+admin = name;
+alert(admin);
+
+let planetEarth = "Земля!";
+let currentUserName = "Джон";
+
+const BIRTHDAY = '15.02.2002';
+const age = someCode(BIRTHDAY);
