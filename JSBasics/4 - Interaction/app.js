57d658a37e07c482e91f8f93c352422bd3ac064e
DorinGavrilita <gavrilitadorin5@gmail.com>
2023-03-05 20:37:01 +0200

    Interaction

---
+++ JSBasics/4 - Interaction/app.js
@@ -0,0 +1,8 @@
+let yourName = prompt('What is your name?');
+alert(`Your name is ${yourName}.`);
+
+let age = prompt('How old are you?');
+alert(`You are ${age} years old!`);
+
+let isHealthy = confirm('Are you healthy?');
+alert(isHealthy);
