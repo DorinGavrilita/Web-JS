b9620537e98829e3be540b8b90df5b289715d6cb
DorinGavrilita <gavrilitadorin5@gmail.com>
2023-03-07 21:33:57 +0200

    Comparison Operators

--- JSBasics/5 - Operators/app.js
+++ JSBasics/5 - Operators/app.js
@@ -59,8 +59,38 @@ console.log(undefined + 1) // 'nan'
 console.log(" \t \n" - 2) // '-2'
 
 // 4 	Исправьте сложение.		Результат должен быть 3	
-a = prompt("Первое число?", 1);
-b = prompt("Второе число?", 2);
+// a = prompt("Первое число?", 1);
+// b = prompt("Второе число?", 2);
 
-alert(a + b); // '12'
-alert(+a + +b); // 3
+// alert(a + b); // '12'
+// alert(+a + +b); // 3
+
+// Операторы сравнения
+console.log('Результат сравнения имеет логический тип');
+console.log(2 > 0);
+console.log(3 > 9);
+console.log(7 == 5);
+
+let result = 15 > 7;
+console.log(result);
+
+// Сравнение строк
+console.log('Сравнение строк\n', 'Стипендия' > 'Зарплата'); // true
+console.log('ДЕньги' < 'день'); // true
+console.log('EVIL' > 'good'); // false
+
+// Сравнение разных типов
+console.log('Сравнение разных типов\n', '2' > 1);
+console.log('3' == 3); // true
+
+// Строгое сравнение
+console.log('Строгое сравнение\n', 0 === false);
+
+console.log('Задачи');
+console.log(5 > 4); // true
+console.log("ананас" > "яблоко");// false
+console.log("2" > "12"); // true
+console.log(undefined == null); // true
+console.log(undefined === null); // false
+console.log(null == "\n0\n"); // false
+console.log(null === +"\n0\n"); // false
