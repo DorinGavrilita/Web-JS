c8e13734ed07fc4eeb7e550455e898ca3d694855
DorinGavrilita <gavrilitadorin5@gmail.com>
2023-03-09 21:31:41 +0200

    add slidesPlugin

--- Result school/marathon-urok-1/app.js
+++ Result school/marathon-urok-1/app.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide')
+const slidesPlugin = (activeSlide = 0) => {
+	const slides = document.querySelectorAll('.slide')
 
-for(const slide of slides){
-	slide.addEventListener('click', () => {
-		clearActiveClasses()
+	slides[activeSlide].classList.add('active')
 
-		slide.classList.add('active')
-	})
+	for (const slide of slides) {
+		slide.addEventListener('click', () => {
+			clearActiveClasses()
+
+			slide.classList.add('active')
+		})
+	}
+
+	const clearActiveClasses = () => {
+		slides.forEach((slide) => {
+			slide.classList.remove('active')
+		})
+	}
 }
 
-const clearActiveClasses = () => {
-	slides.forEach((slide) => {
-		slide.classList.remove('active')
-	})
-}
+slidesPlugin()
