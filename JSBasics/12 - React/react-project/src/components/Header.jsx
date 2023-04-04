82d20ee63d8c99766094bdf36472f432618d6eea
DorinGavrilita <gavrilitadorin5@gmail.com>
2023-04-04 20:31:47 +0300

    React Framework

---
+++ JSBasics/12 - React/react-project/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header(){
+	return (
+		<header>
+			<nav>
+				<a href="#">Home</a>
+				<a href="#">Photos</a>
+				<a href="#">Videos</a>
+				<a href="#">Blog</a>
+				<a href="#">Contacts</a>
+			</nav>
+		</header>
+	)
+}
